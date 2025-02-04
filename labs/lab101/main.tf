bc3c71155b09727f96aa44e48b1330260e093d3b
Your Name <you@example.com>
2025-02-04 12:37:36 +0000

    Lab1 With amazon cmd

--- labs/lab101/main.tf
+++ labs/lab101/main.tf
@@ -37,3 +37,6 @@ output "vm_public_ip" {
   value       = aws_instance.vm.public_ip
   description = "Public IP address of the VM"
 }
+
+# to see all the details. 
+#aws ec2 describe-instances --instance-ids i-0919a052c41cf3309 --query "Reservations[*].Instances[*].[InstanceId, PublicIpAddress, State.Name, InstanceType, SecurityGroups[*].GroupId]" --output table
