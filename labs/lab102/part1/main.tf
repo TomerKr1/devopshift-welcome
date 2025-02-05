dafb49819e0c3b55d42e4f29cfc5e159f2dbdc85
Your Name <you@example.com>
2025-02-05 09:48:34 +0000

    lab102

--- labs/lab102/part1/main.tf
+++ labs/lab102/part1/main.tf
@@ -6,6 +6,9 @@ variable "region" {
   default = "us-east-1"
 }
 
+variable "publicID" {
+  default = ""
+}
 
 /*terraform {
   required_providers {
@@ -16,16 +19,21 @@ variable "region" {
   }
 }*/
 
+
 resource "null_resource" "check_public_ip" {
   provisioner "local-exec" {
     command = <<EOT
-      if [ -z "${aws_instance.vm.public_ip}" ]; then
+      if [ -z "${var.publicID}" ]; then
         echo "ERROR: Public IP address was not assigned." >&2
-        exit 1
       fi
+
+        echo "im trying again..."
+        if [ -n "${aws_instance.vm.public_ip}" ]; then
+          echo "you got an amazing ip!"
+        fi
+      
     EOT
   }
-
   depends_on = [aws_instance.vm]
 }
 
@@ -56,16 +64,16 @@ resource "aws_instance" "vm" {
   }
 }
 
-/*resource "time_sleep" "wait_for_ip" {
-  create_duration = "45s"  # Introduce a delay of 30 seconds
-}*/
+
 
 
 output "vm_public_ip" {
   value       = aws_instance.vm.public_ip
   description = "Public IP address of the VM"
-  ##depends_on = [ time_sleep.wait_for_ip]
+  depends_on = [null_resource.check_public_ip]
 }
+
+
 
 # to see all the details. 
 #aws ec2 describe-instances --instance-ids i-0919a052c41cf3309 --query "Reservations[*].Instances[*].[InstanceId, PublicIpAddress, State.Name, InstanceType, SecurityGroups[*].GroupId]" --output table
