0e580777abc1d25f94ecfb72248a04664003471a
Your Name <you@example.com>
2025-02-06 09:12:17 +0000

    Builder

--- labs/labl500/labmodule/main.tf
+++ labs/labl500/labmodule/main.tf
@@ -1,4 +1,4 @@
-module "EC2Builder" {
+module "EC2Builder_1" {
   source = "./moudles/ec2"
   ami = "ami-0c02fb55956c7d316"
   machinetype = "t2.micro"
@@ -6,15 +6,15 @@ module "EC2Builder" {
 }
 
 output "print_module_ami" {
-  value = module.EC2Builder.print_ami
+  value = module.EC2Builder_1.print_ami
 }
 output "print_module_publicIP" {
-  value = module.EC2Builder.vm_public_ip
+  value = module.EC2Builder_1.vm_public_ip
 }
 
 output "print_machineName" {
-  value = module.EC2Builder.print_machine_name
+  value = module.EC2Builder_1.print_machine_name
 }
 output "test" {
-  value=module.EC2Builder.DockDock
+  value=module.EC2Builder_1.DockDock
 }
