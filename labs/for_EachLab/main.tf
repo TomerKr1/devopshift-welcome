4c09fff11421aaf671187dbf1297901cca111e65
Your Name <you@example.com>
2025-02-06 11:55:00 +0000

    With extra way to do for each

--- labs/for_EachLab/main.tf
+++ labs/for_EachLab/main.tf
@@ -9,12 +9,12 @@ variable "s3_buckets" {
 }
 
 resource "aws_s3_bucket" "buckets" {
-  for_each = { for service in var.s3_buckets : service => service if contains(var.enabled_services, service) }
+  #for_each = { for service in var.s3_buckets : service => service if contains(var.enabled_services, service) }
 
-#for_each = toset([for service in var.s3_buckets : service if contains(var.enabled_services, service)])
+for_each = toset([for service in var.s3_buckets : service if contains(var.enabled_services, service)])
 
   bucket = "my-app-${each.key}"
-  acl    = "private"
+
 
   tags = {
     Name        = "Bucket for ${each.key}"
