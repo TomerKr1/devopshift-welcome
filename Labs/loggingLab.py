1fd4da419f00ad04ba01d2efb4f1e6b6f5808e60
Tomer <tomer.kraft1@gmail.com>
2025-02-10 11:39:57 +0200

    Update

--- Labs/loggingLab.py
+++ Labs/loggingLab.py
@@ -1,3 +1,25 @@
 import logging
 
-logging.
+'''
+ERROR
+WARN
+INFO 
+DEBUG
+'''
+
+
+logging.basicConfig(filename="loggingLab.log",format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+logger = logging.getLogger("Tomer")
+logger.setLevel(logging.DEBUG)
+
+logger.info("This is an info message")
+
+new_logger = logging.getLogger("Tomer2")
+new_logger.setLevel(logging.DEBUG)
+new_logger.info("This is an info message from the new logger")
+
+
+
+logger.warning("This is a warning message")
+logger.error("This is an error message")
