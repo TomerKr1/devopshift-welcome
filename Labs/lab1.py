93c18672f65927b3b7201a189374699267765a83
Tomer <tomer.kraft1@gmail.com>
2025-02-10 11:11:17 +0200

    lab1

--- Labs/lab1.py
+++ Labs/lab1.py
@@ -3,9 +3,23 @@ import random
 server_names = [
     "light",
     "nginx",
-    "docker"
+    "docker", 
+    "apache",
+    "mysql",
+    "postgresql",
+    "redis",
+    "memcached",
+    "haproxy",
+    "varnish",
+    "cassandra",
+    "mongodb",
+    "rabbitmq",
 ]
+
+
 server_status = {server: random.choice([True, False]) for server in server_names}
+print("---------------labl1-----------------")
+
 
 
 value1 = input("Enter the server name: ")
@@ -17,7 +31,7 @@ except Exception as e:
 if value1 in server_names:
     print("The server exists.")
 else:
-    print("The server does not exist.")
+    raise ValueError(f"{value1} is not a recognized server.")
     
 print("---------------labl2-----------------")
 if value1 in server_status:
