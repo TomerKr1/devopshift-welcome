bc23ddfccbeb1128629bd12cbc502eeeb4048952
Tomer <tomer.kraft1@gmail.com>
2025-02-11 09:24:54 +0200

    new

---
+++ Labs/lab02.py
@@ -0,0 +1,6 @@
+# lambda function
+
+
+
+somthing2 = lambda x,y: x+y
+print(somthing2(3,4))
---
+++ Labs/stringformat.py
@@ -0,0 +1,5 @@
+x="rwer"
+y="dss"
+
+stringst="tomer{valuex} and the {vALUEY}".format(valuex=x,vALUEY=4)
+print(stringst)
